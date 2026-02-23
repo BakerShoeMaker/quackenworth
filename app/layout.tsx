0cfb3b99cd34fb757d8c171f48b9d163f105b6ed
BakerShoeMaker <info@quackenworth.com>
2026-02-22 20:07:25 -0800

    fixed footer to stay at the bottom of the page.

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} antialiased bg-white text-gray-900`}>
+      <body className={`${geistSans.variable} antialiased bg-white text-gray-900 flex flex-col min-h-screen`}>
         <Header />
-        {children}
+        <div className="flex-1">{children}</div>
         <Footer />
       </body>
     </html>
